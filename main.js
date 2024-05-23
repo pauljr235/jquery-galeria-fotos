bb08f7249b7ed1bec559f97df629d0dbf941d632
Paulo <pauljr235@gmail.com>
2024-05-23 17:19:38 +0100

    primeiro commit

---
+++ main.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    /*SELECIONANDO OS MESMOS ELEMENTOS COM JQUERY*/
+    $('header button').click(function(){
+        $('form').slideDown();
+    })
+
+    $('#botao-cancelar').click(function(){
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+        const novoItem = $('<li style="display: none"></li>');
+        $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
+        $(`
+            <div class="overlay-imagem-link">
+                <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
+                    Ver imagem em tamanho real
+                </a>
+            </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+        $('#endereco-imagem-nova').val('')
+    })
+
+});
+
+
+    /*SELECIONANDO OS ELEMENTOS COM JS
+
+    console.log(document.querySelector('header button'))
+    console.log($('#botao-cancelar'))
+    document.querySelector('header button').addEventListener('click', function(){
+    })*/
+
+    
